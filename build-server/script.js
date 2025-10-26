57db2a8f4db41ac112dfc7db8e8a314bd96908f6
GaganshuYadav <vishu16aims@gmail.com>
2025-10-26 11:25:03 +0530

    add build script logic check

--- build-server/script.js
+++ build-server/script.js
@@ -1,7 +1,6 @@
-// GITHUB_REPOSITORY="https://github.com/Gaganshuyadav/template-for-simple-node-server-ts.git"
-// REPO_ID="1234"
-
-
+const { exec} = require("child_process");
+const path = require("path");
+const fs = require("fs");
 
 BuiidRepo = ()=>{
     // console.log("I Am Ironman");
@@ -9,8 +8,8 @@ BuiidRepo = ()=>{
     // console.log(process.env.REPO_ID);
     // console.log("end");
 
-    const github_repository_url = process.env.GITHUB_REPOSITORY_URL;
-    const repo_id = process.env.REPO_ID;
+    const github_repository_url = process.env.GITHUB_REPOSITORY_URL || "https://github.com/dumindapriyasad/todo-app.git";
+    const repo_id = process.env.REPO_ID || `${(Math.floor(Math.random()*1000))}`;
 
     if(!(github_repository_url)){
         console.log("GITHUB_REPOSITORY_URL not provided");
@@ -22,9 +21,43 @@ BuiidRepo = ()=>{
         return new Error("REPO_ID not provided")
     }
 
-    const clone = exec(`git clone ${github_repository_url}`);
+    const folderPathForRepoClone = path.join(__dirname,`./cloned-repo/${repo_id}`); 
+    const repoName = github_repository_url.split("/")[github_repository_url.split("/").length-1].toString().split(".")[0];
+
+    const clone = exec(`git clone ${github_repository_url} ${folderPathForRepoClone}`);
 
     console.log("cloning.....");
+
+    clone.on("close", ()=>{
+
+        // make path to check if it contains build command or not
+        const targetPath = path.join(__dirname, `./cloned-repo/${repo_id}/`);
+
+        const pkgPath = path.join(targetPath,"package.json");
+
+        console.log(pkgPath);
+
+        // if path exist for build Script
+        if(fs.existsSync(pkgPath)){
+
+            const pkg = JSON.parse(fs.readFileSync(pkgPath,"utf-8"));
+
+            const hasBuildScript = pkg.scripts && pkg.scripts.build;
+
+            if(hasBuildScript){
+                console.log("contains build script");
+            }
+            else{
+                console.log("contains package.json but missing `build` script");
+                return new Error("contains package.json but missing `build` script");
+            }
+        }
+        //simple html,css,js files
+        else{
+
+        }
+
+    })
 
 
     
