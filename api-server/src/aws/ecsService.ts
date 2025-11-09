45de0910b967a18f6c007d686e1289cebd5c689b
GaganshuYadav <vishu16aims@gmail.com>
2025-11-09 17:56:23 +0530

    add ECS Fargate logic for creating task definitions and limits

--- api-server/src/aws/ecsService.ts
+++ api-server/src/aws/ecsService.ts
@@ -1,2 +1,40 @@
+import { ECS, ListTasksCommand, RunTaskCommand, type ListClustersCommandOutput, type ListTasksCommandOutput, type RunTaskCommandInput } from "@aws-sdk/client-ecs";
 
-//implement in future
+const ecsClient = new ECS({
+    region: process.env.AWS_REGION as string,
+    credentials:{
+        accessKeyId: process.env.AWS_ACCESS_KEY as string,
+        secretAccessKey: process.env.AWS_SECRET_KEY as string
+    }
+})
+
+class ECS_Service{
+
+    public async runSingleNewTask( taskParams:RunTaskCommandInput){
+
+    const command = new RunTaskCommand(taskParams);
+    const ecsResponse  = await ecsClient.send(command);
+    return ecsResponse;
+
+    }
+
+    public async getRunningTaskCount( clusterName:string){
+        //List all running tasks in the cluster
+
+        const command:ListTasksCommand = new ListTasksCommand({
+            cluster: clusterName,
+            desiredStatus: "RUNNING"
+        })
+
+        const tasksList:ListTasksCommandOutput = await ecsClient.send(command);
+        const runningCount =  tasksList.taskArns?.length ?? 0;
+        console.log(`Currently running tasks Count : ${runningCount}`);
+
+        return runningCount;
+    }
+
+}
+
+const ecsService = new ECS_Service();
+
+export { ecsService};
--- api-server/src/aws/sqsService.ts
+++ api-server/src/aws/sqsService.ts
@@ -1,4 +1,6 @@
-import { DeleteMessageCommand, ReceiveMessageCommand, SendMessageCommand, SQS} from "@aws-sdk/client-sqs";
+import { DeleteMessageCommand, ReceiveMessageCommand, SendMessageCommand, SQS, type ReceiveMessageCommandOutput} from "@aws-sdk/client-sqs";
+import { ecsService } from "./ecsService.js";
+import type { AssignPublicIp, AwsVpcConfiguration, LaunchType } from "@aws-sdk/client-ecs";
 
 const sqsClient = new SQS({
     region: process.env.AWS_REGION as string,
@@ -23,35 +25,68 @@ export const sendMessage = async( message:object)=>{
 
 
 //consumer
-export const pollMessages = async ( pollTime:number=5000)=>{
+export const pollChecker = async ( pollTime:number=5000)=>{
 
-    console.log(" Polling messages... ");
+    console.log(" Polling checker started... ");
 
-    setInterval( async ()=>{
+    setTimeout( async ()=>{
 
         const receiveParams = {
             QueueUrl: process.env.AWS_QUEUE_URL,
             MaxNumberOfMessages: 2
         }
 
-        try{
+        const runningTaskCount = await ecsService.getRunningTaskCount( process.env.AWS_ECS_BUILD_CLUSTER_NAME as string);
 
+        // if already at or above limit -> skip polling
+        if( runningTaskCount >= 200){
+            console.log("Max Concurrency reached. Skipping new messages");
+            return;
+        }
+
+        // safe to poll messages from SQS
+        try{
+            console.log("Receiving Messages... ");
             const receiveCommand = new ReceiveMessageCommand(receiveParams);
-            const sqsReceiveData = await sqsClient.send(receiveCommand);
+            const sqsReceiveData:ReceiveMessageCommandOutput = await sqsClient.send(receiveCommand);
             console.log("SQS Received Data: ",sqsReceiveData?.Messages ? sqsReceiveData?.Messages?.length : 0 );
 
-            if(sqsReceiveData.Messages && sqsReceiveData.Messages.length>0){
-                for(let i=0; i<sqsReceiveData.Messages.length; i++){
+            if( sqsReceiveData?.Messages && sqsReceiveData?.Messages?.length > 0){
+                for(let i=0; i<sqsReceiveData?.Messages?.length; i++){
 
                     if( sqsReceiveData.Messages[i]?.Body){
                         console.log("this is received message", JSON.parse(sqsReceiveData.Messages[i]?.Body as string));
 
-                        //promise of 15 seconds act as ECS task
-                        await new Promise(( resolve, reject)=>{
-                            setTimeout(()=>{
-                                resolve("resolved");
-                            },15000);
+                        // ECS task
+                        const ecsResponse = await ecsService.runSingleNewTask({
+                            cluster: process.env.AWS_ECS_BUILD_CLUSTER_NAME,
+                            taskDefinition: process.env.AWS_ECS_BUILD_TASK_DEFINITION_NAME,
+                            launchType: process.env.AWS_ECS_LAUNCH_TYPE as LaunchType,
+                            networkConfiguration: {
+                                awsvpcConfiguration: {
+                                    assignPublicIp: process.env.AWS_ECS_BUILD_ASSIGN_PUBLIC_IP as AssignPublicIp,
+                                    subnets: JSON.parse(process.env.AWS_ECS_SECURITY_GROUPS_LIST || '[]') as Array<string>,
+                                    securityGroups: JSON.parse(process.env.AWS_ECS_SECURITY_GROUPS_LIST || '[]') as Array<string>
+                                }
+                            },
+                            overrides: {
+                                containerOverrides: [
+                                    {
+                                        name: process.env.REPO_ID,
+                                        environment: [
+                                            { name: "GITHUB_REPOSITORY_URL", value: process.env.GITHUB_REPOSITORY_URL },
+                                            { name: "REPO_ID", value: process.env.REPO_ID},
+                                            { name: "AWS_ACCESS_KEY", value: process.env.AWS_ACCESS_KEY },
+                                            { name: "AWS_SECRET_KEY", value: process.env.AWS_SECRET_KEY },
+                                            { name: "AWS_REGION", value: process.env.AWS_REGION },
+                                            { name: "AWS_BUCKET", value: process.env.AWS_BUCKET },
+                                        ]
+                                    }
+                                ]
+                            }
                         })
+
+                        console.log("ECS Response: ",ecsResponse);
 
                         //delete message from queue
                         await deleteMessage(sqsReceiveData.Messages[i]?.ReceiptHandle as string);
@@ -65,11 +100,12 @@ export const pollMessages = async ( pollTime:number=5000)=>{
         catch(err){
             console.log("Error Receiving Messages: ", err);
         }
+
     }, pollTime);
 }
 
-//run poll messages function
-pollMessages(5000);
+//run poll checker function
+pollChecker(15000);
 
 //delete from queue
 export const deleteMessage = async ( ReceiptHandle:string)=>{
