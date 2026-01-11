851762d73d18b42226eceae21c2bacc4ed46f9e1
GaganshuYadav <vishu16aims@gmail.com>
2026-01-11 09:11:02 +0530

    add user and auth service prototype

---
+++ api-server/src/services/user.service.ts
@@ -0,0 +1,34 @@
+import type { NextFunction, Response, Request } from "express";
+import catchAsyncErrors from "../middleware/catch-async.js";
+import { prisma } from "../DB/prisma-client/PrismaClient.js";
+import type { User } from "../generated/prisma/client.js";
+
+class UserService{
+
+    public createUser = async ( body:User):Promise<any> =>{
+        
+        const newUser = await prisma.user.create({
+            data: {
+                firstName: body.firstName,
+                lastName: body.lastName,
+                email: body.email,
+                password: body.password
+            }
+        })
+
+        return newUser;
+    }
+
+    public getUser = async ( id:string):Promise<any> =>{
+        
+
+    } 
+
+    public getAllUsers = async ( ):Promise<any> =>{
+        return
+    }
+}
+
+const userService = new UserService();
+
+export { userService};
