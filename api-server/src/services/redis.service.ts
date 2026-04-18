9d387f1d051d31d2fb1f31a4a52ec3bed77f7e58
GaganshuYadav <vishu16aims@gmail.com>
2026-04-18 18:26:15 +0530

    add one instance logic

--- api-server/src/services/redis.service.ts
+++ api-server/src/services/redis.service.ts
@@ -1,20 +1,26 @@
 import { redisSub } from "../config/redisClient.js";
+import type { KafkaMessageRawLogEvent } from "../types/interfaces/clickhouse_log_event_schema.js";
 import { sseService } from "./sseService.js";
 
+let isRedisSubscriberStarted = false;
 
 async function startRedisSubscriber() {
+
+    if (isRedisSubscriberStarted) {
+        console.log("Redis subscriber already started");
+        return;
+    }
 
     const ss = await redisSub?.psubscribe("sse-publish-logs");
 
     redisSub?.on("pmessage", ( pattern, channel, message)=>{
 
-        const messageData = JSON.parse(message || "");
+        const messageData:KafkaMessageRawLogEvent = JSON.parse(message || "");
         console.log(":::::::: ", messageData);
 
-        if(!messageData?.deploymentId) return; 
+        if(!messageData?.deployment_id) return; 
 
-        // sendLogsToUser( deploymentId:string, logData:any)
-        sseService.sendLogsToUser( `${messageData?.deploymentId}`, messageData);
+        sseService. sendLogsToUser( messageData.deployment_id, messageData.message);
         
     })
 
